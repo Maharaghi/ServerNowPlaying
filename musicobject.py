e4bdd829cfa6befbfb811baa2d5b8c29aaa5d8d8
Mjau <mjau@paranta.se>
2021-05-30 14:01:58 +0200

    fixed path

--- musicobject.py
+++ musicobject.py
@@ -45,7 +45,7 @@ class MusicObject:
 
         if self.image:
             # This will set the url to the thumbnail we have saved
-            obj['image'] = 'thumbnail/{}/thumb.jpg'.format(self.id)
+            obj['image'] = '/thumbnail/{}/thumb.jpg'.format(self.id)
             # This will send the entire image as base64
             # obj['image'] = 'data:image/png;base64,' + b64encode(self.image.getvalue()).decode()
 
